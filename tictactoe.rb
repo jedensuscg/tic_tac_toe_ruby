4f28b0d9d9b82cebdce5784a77edab6bbdd38883
James Edens <jedensuscg@gmail.com>
2020-03-14 19:29:51 -0400

    created grid generation and changing of squares

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,17 +1,45 @@
 class Player
-    attr_reader :name, :marker
+    attr_accessor :name, :marker
     @@player = 0
 
     def initialize(name,marker)
         @name = name
         @marker = marker
         @@player += 1
-
     end
 end
 
 class Board
-   # @@board = {:r1 => {:c1 => ".", :c2 => ".", :c3}, :r2 => {:c1 => ".", :c2 => ".", :c3}, :r3 => {:c1 => ".", :c2 => ".", :c3}}
+    attr_reader :board
+    def initialize
+        @board = {:r1 => {:c1 => "X", :c2 => ".", :c3 => "X"}, :r2 => {:c1 => ".", :c2 => ".", :c3 => "O"}, :r3 => {:c1 => ".", :c2 => ".", :c3 => "."}}
+    end
+
+    def printBoard()
+        line = "\u2500"
+        count = 0
+        @board.each do |row, cols|
+            puts "#{cols[:c1]} | #{cols[:c2]} | #{cols[:c3]}"
+            if count < 2
+                puts line + line + line + line + line + line + line + line + line + line
+            end
+            count += 1
+        end
+    end 
+
+    def updateBoard(player, position)
+        row = "r" + position[0]
+        col = "c" + position[1]
+        @board[row.to_sym][col.to_sym] = player.marker
+        printBoard()
+    end
+
+    def CheckForWin
+       check_array = []
+       @board.each do |row, cols|
+            row.each |col, val|
+        end
+    end
 end
 
 def CreatePlayers
@@ -55,15 +83,21 @@ def GetMarker(name)
         end
     end
 end
+board = Board.new
 
-board = {:r1 => {:c1 => ".", :c2 => ".", :c3 => "."}, :r2 => {:c1 => ".", :c2 => ".", :c3 => "."}, :r3 => {:c1 => ".", :c2 => ".", :c3 => "."}}
 
-board.each do |row, col|
-    
-end
+
+
+
+
+board.printBoard()
+
 CreatePlayers()
 
 puts "------------------"
 puts "Start game with these settings?"
 puts "Player One: #{$player1.name} using marker '#{$player1.marker}''"
 puts "Player One: #{$player2.name} using marker '#{$player2.marker}''"
+
+board.updateBoard($player2, "12")
+board.CheckForWin()
